290a29138f8f00d52c2a8e4af4966728590b6c15
Stephen Muss <stephenmuss@gmail.com>
2019-07-18 20:13:14 +1000

    Improved example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,13 +1,18 @@
+#[macro_use] extern crate prettytable;
+extern crate chrono;
+
 use std::sync::Arc;
 use std::error::Error;
 
+use chrono::{Utc, TimeZone};
+use futures::Stream;
 use grpcio::{ChannelBuilder, EnvBuilder, CallOption};
+use grpcio::MetadataBuilder;
+use prettytable::{Table, format};
 
 use helm_api::status::Status_Code;
 use helm_api::tiller;
 use helm_api::tiller_grpc;
-use futures::Stream;
-use grpcio::MetadataBuilder;
 
 const HELM_VERSION: &'static str = "2.13.1";
 const TILLER_HOST: &'static str = "localhost:44134";
@@ -28,15 +33,32 @@ fn main() -> Result<(), Box<Error>> {
         Status_Code::PENDING_UPGRADE,
         Status_Code::PENDING_ROLLBACK,
     );
+
     let mut m = MetadataBuilder::with_capacity(1);
     m.add_str("x-helm-api-client", HELM_VERSION)?;
     let opts = CallOption::default().headers(m.build());
+
+    let mut table = Table::new();
+    table.add_row(row!["NAME", "REVISION", "UPDATED", "STATUS", "CHART", "APP VERSION", "NAMESPACE"]);
+    table.set_format(*format::consts::FORMAT_CLEAN);
+
     for response in client.list_releases_opt(&req, opts)?.wait() {
         for release in  response?.releases.into_iter() {
-            let info = release.info.unwrap() ;
-            let status = info.status.unwrap() ;
-            println!("{} | {} | {:?} | {}", release.name, release.version, status.code, release.namespace);
+            let info = release.get_info();
+            let chart = release.get_chart();
+            let metadata = chart.get_metadata();
+            let last_deployed = info.get_last_deployed();
+            table.add_row(row![
+                release.name,
+                release.version,
+                Utc.timestamp(last_deployed.seconds, 0),
+                format!("{:?}", info.get_status().code),
+                metadata.name,
+                metadata.appVersion,
+                release.namespace,
+            ]);
         }
     }
+    table.printstd();
     Ok(())
 }
